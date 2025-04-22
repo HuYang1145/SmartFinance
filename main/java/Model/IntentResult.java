9a4055a0f3f0328981f3c8da450f7078c07833cf
ajiuyq <135525842+ajiuyq@users.noreply.github.com>
2025-04-22 21:46:17 +0800

    Delete main/java directory

--- main/java/Model/IntentResult.java
+++
@@ -1,8 +0,0 @@
-package Model;
-
-public class IntentResult {
-    private String intent;
-    public String getIntent() { return intent; }
-    public void setIntent(String intent) { this.intent = intent; }
-}
-
